08e6c04fd0e764e4818df084680a750e65350e3a
Zekun Lin <zlin22@wolftech.ad.ncsu.edu>
2020-10-19 23:25:02 -0400

    create netcdf for each year, and each year will have 7 bands(b1-b7, and qa band).

--- ard_process_data.r
+++ ard_process_data.r
@@ -5,108 +5,103 @@ data_repo <- "D:/Zekun/Landsat_ARD/Landsat_ARD/wake_ard_period2/"
 library(raster)
 library(stringr)
 library(matrixStats)
+library(ncdf4)
+library(foreach)
+library(doParallel)
+## explore and store data in netcdf format
 
-## now do the image composite by seasons based on DOY
+b1 <- list.files(data_repo, pattern = "^.*SRB1.*", full.names = T)
+b2 <- list.files(data_repo, pattern = "^.*SRB2.*", full.names = T)
+b3 <- list.files(data_repo, pattern = "^.*SRB3.*", full.names = T)
+b4 <- list.files(data_repo, pattern = "^.*SRB4.*", full.names = T)
+b5 <- list.files(data_repo, pattern = "^.*SRB5.*", full.names = T)
+b7 <- list.files(data_repo, pattern = "^.*SRB7.*", full.names = T)
+bqa <- list.files(data_repo, pattern = "^.*PIXELQA.*", full.names = T)
 
-img_date <- str_extract(b1_11, '[0-9][0-9][0-9][0-9][0-9][0-9][0-9]') # get doy of each image
-yr <- 2011
-origins<- paste(yr, "-01-01", sep = "")
-substr(img_date, 5, 7)
-img_mth_days <- as.Date(as.numeric(substr(img_date, 5,7)), origin = origins)
-mth_numbers <- as.numeric(format(img_mth_days, "%m"))
+cores=detectCores()
+cl <- makeCluster(cores[1]-1) #not to overload your computer
+registerDoParallel(cl)
+foreach(yr = 1995:2011) %dopar% {
+    b1 <- list.files(data_repo, pattern = paste("^.*SRB1.doy", yr, sep = ""), full.names = TRUE)
+    b2 <- list.files(data_repo, pattern = paste("^.*SRB2.doy", yr, sep = ""), full.names = TRUE)
+    b3 <- list.files(data_repo, pattern = paste("^.*SRB3.doy", yr, sep = ""), full.names = TRUE)
+    b4 <- list.files(data_repo, pattern = paste("^.*SRB4.doy", yr, sep = ""), full.names = TRUE)
+    b5 <- list.files(data_repo, pattern = paste("^.*SRB5.doy", yr, sep = ""), full.names = TRUE)
+    b7 <- list.files(data_repo, pattern = paste("^.*SRB7.doy", yr, sep = ""), full.names = TRUE)
+    bqa <- list.files(data_repo, pattern = paste("^.*PIXELQA.doy", yr, sep = ""), full.names = TRUE)
 
-s1_ind <- which(mth_numbers <= 3)
-s2_ind <- which(mth_numbers <= 6 & mth_numbers > 3)
-s3_ind <- which(mth_numbers <= 8 & mth_numbers > 6)
-s4_ind <- which(mth_numbers <= 12 & mth_numbers >8)
-seasons_ind <- list(s1 = s1_ind, s2 = s2_ind, s3 = s3_ind, s4 = s4_ind)
-##############################################################################################
+    b1_stack <- stack(b1)
+    b1.array <- array(b1_stack, dim = dim(b1_stack))
+    b2_stack <- stack(b2)
+    b2.array <- array(b2_stack, dim = dim(b2_stack))
+    b3_stack <- stack(b3)
+    b3.array <- array(b3_stack, dim = dim(b3_stack))
+    b4_stack <- stack(b4)
+    b4.array <- array(b4_stack, dim = dim(b4_stack))
+    b5_stack <- stack(b5)
+    b5.array <- array(b5_stack, dim = dim(b5_stack))
+    b7_stack <- stack(b7)
+    b7.array <- array(b7_stack, dim = dim(b7_stack))
+    qa_stack <- stack(bqa)
+    qa.array <- array(qa_stack, dim = dim(qa_stack))
 
-## now we are getting files for all sr data
-b1 <- list.files(data_repo, pattern = "SRB1", full.names = T)
-b2 <- list.files(data_repo, pattern = "SRB2", full.names = T)
-b3 <- list.files(data_repo, pattern = "SRB3", full.names = T)
-b4 <- list.files(data_repo, pattern = "SRB4", full.names = T)
-b5 <- list.files(data_repo, pattern = "SRB5", full.names = T)
-b7 <- list.files(data_repo, pattern = "SRB7", full.names = T)
-bqa <- list.files(data_repo, pattern = "PIXELQA_doy", full.names = T)
+   
+    lon <- xFromCol(b1.layer1)
+    lat <- yFromRow(b1.layer1)
+    time <- str_extract(b1, '[0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
+    crs.info <- as.character(crs(b1_stack))
+    extent.info <-  extent(b1_stack)
+    res.info <- as.character(res(b1_stack))
+    varname <- 'sr'
+    Date.range <- c(time[1], tail(time, n=1))
 
-b1_11 <- list.files(data_repo, pattern = "^.*SRB1.doy2011.*", full.names = T)
-b2_11 <- list.files(data_repo, pattern = "^.*SRB2.doy2011.*", full.names = T)
-b3_11 <- list.files(data_repo, pattern = "^.*SRB3.doy2011.*", full.names = T)
-b4_11 <- list.files(data_repo, pattern = "^.*SRB4.doy2011.*", full.names = T)
-b5_11 <- list.files(data_repo, pattern = "^.*SRB5.doy2011.*", full.names = T)
-b7_11 <- list.files(data_repo, pattern = "^.*SRB7.doy2011.*", full.names = T)
-bqa_11 <- list.files(data_repo, pattern = "^.*PIXELQA.doy2011.*", full.names = T)
+    ncpath <- "D:/Zekun/Imp_classification/ard_nc_files/"
+    ncname <- paste("LT05_Wake_ncdf4_", yr, ".nc", sep = "")
+    file_name <- paste(ncpath, ncname, sep = "")
+    data_name <- 'sr'
+    londim <- ncdim_def("lon","degrees_east", as.double(lon))
+    latdim <- ncdim_def("lat", "degrees_north", as.double(lat))
+    timedim <- ncdim_def("time", "day_of_year", as.double(time))
+    fillvalue <- NA
+    data_long_name <- "surface_reflectance"
 
-# stack and convert to matrix
-b1_stack <- stack(b1_11)
-b1_matrix <- as.matrix(b1_stack)
-b2_stack <- stack(b2_11)
-b2_matrix <- as.matrix(b2_stack)
-b3_stack <- stack(b3_11)
-b3_matrix <- as.matrix(b3_stack)
-b4_stack <- stack(b4_11)
-b4_matrix <- as.matrix(b4_stack)
-b5_stack <- stack(b5_11)
-b5_matrix <- as.matrix(b5_stack)
-b7_stack <- stack(b7_11)
-b7_matrix <- as.matrix(b7_stack)
+    b1_def <- ncvar_def(name = "b1_sr", units = "", dim = list(londim, latdim, timedim), fillvalue, data_long_name, prec = "single")
+    b2_def <- ncvar_def(name = "b2_sr", units = "", dim = list(londim, latdim, timedim), fillvalue, data_long_name, prec = "single")
+    b3_def <- ncvar_def(name = "b3_sr", units = "", dim = list(londim, latdim, timedim), fillvalue, data_long_name, prec = "single")
+    b4_def <- ncvar_def(name = "b4_sr", units = "", dim = list(londim, latdim, timedim), fillvalue, data_long_name, prec = "single")
+    b5_def <- ncvar_def(name = "b5_sr", units = "", dim = list(londim, latdim, timedim), fillvalue, data_long_name, prec = "single")
+    b7_def <- ncvar_def(name = "b7_sr", units = "", dim = list(londim, latdim, timedim), fillvalue, data_long_name, prec = "single")
+    bqa_def <- ncvar_def(name = "qa", units = "", dim = list(londim, latdim, timedim), fillvalue, data_long_name, prec = "single")
 
-b1_matrix_copy <- b1_matrix
-bqa_stack <- stack(bqa_11)
-qa_matrix <- as.matrix(bqa_stack)
+    ncout <- nc_create(file_name, list(b1_def, b2_def, b3_def, b4_def, b5_def, b7_def, bqa_def), force_v4 =TRUE)
+    ncatt_put(ncout, "lon", "axis", "X")
+    ncatt_put(ncout, "lat", "axis", "Y")
+    ncatt_put(ncout, "time", "axis", "T")
+    ncatt_put(ncout, 0, "CRS", crs.info)
+    ncatt_put(ncout, 0, "cell_size", res.info)
+    ncatt_put(ncout, 0, "xmin", extent.info[1])
+    ncatt_put(ncout, 0, "xmax", extent.info[2])
+    ncatt_put(ncout, 0, "ymin", extent.info[3])
+    ncatt_put(ncout, 0, "ymax", extent.info[4])
 
-bad_pixel_flag <- c(1, 68, 72, 80, 96, 132, 136, 144, 160, 224)
-n_layer <- ncol(b1_matrix)
+    ncvar_put(ncout, b1_def, b1.array)
+    ncvar_put(ncout, b2_def, b2.array)
+    ncvar_put(ncout, b3_def, b3.array)
+    ncvar_put(ncout, b4_def, b4.array)
+    ncvar_put(ncout, b5_def, b5.array)
+    ncvar_put(ncout, b7_def, b7.array)
+    ncvar_put(ncout, bqa_def, qa.array)
 
-for(y in 1:n_layer){
-    # we want the clear pixels only, so remove other pixels keep flag the 66 and 130 and do this for all bands
-    b1_matrix[, y] <- replace(b1_matrix[,y], qa_matrix[,y] %in% bad_pixel_flag, NA)  
-    b2_matrix[, y] <- replace(b2_matrix[,y], qa_matrix[,y] %in% bad_pixel_flag, NA)
-    b3_matrix[, y] <- replace(b3_matrix[,y], qa_matrix[,y] %in% bad_pixel_flag, NA)
-    b4_matrix[, y] <- replace(b4_matrix[,y], qa_matrix[,y] %in% bad_pixel_flag, NA)
-    b5_matrix[, y] <- replace(b5_matrix[,y], qa_matrix[,y] %in% bad_pixel_flag, NA)
-    b7_matrix[, y] <- replace(b7_matrix[,y], qa_matrix[,y] %in% bad_pixel_flag, NA)
+    nc_close(ncout)
 }
+stopCluster(cl)
 
+ncf <- nc_open(file_name)
+nc_close(ncf)
 
-
-sr_allBands_matrix <- list(band1 = b1_matrix, band2 = b2_matrix, band3 = b3_matrix, 
-                            band4 = b4_matrix, band5 = b5_matrix, band7 = b7_matrix)
-ndvi <- function(band3, band4){
-    ndvi_val <- (band4 - band3)/(band4 + band3)
-    return(ndvi_val)
-}
-
-evi <- function(band1, band3, band4){
-    evi_val <- (2.5 * ((band4 - band3)/(band4 + 6*band3 - 7.5*band1 +1)))
-    return(evi_val)
-}
-
-ndvi_matrix <- (sr_allBands_matrix$band4 - sr_allBands_matrix$band3)/(sr_allBands_matrix$band4 + sr_allBands_matrix$band3)
-ndvi_by_season <- list(
-    s1 = rowMedians(ndvi_matrix[,s1_ind]),
-    s2 = rowMedians(ndvi_matrix[,s2_ind]),
-    s3 = rowMedians(ndvi_matrix[,s3_ind]),
-    s4 = rowMedians(ndvi_matrix[,s4_ind])
-)
-
-s1.bandx <- lapply(sr_allBands_matrix, "[", ,seasons_ind[["s1"]])
-s1.med <- lapply(s1.bandx, rowMedians, na.rm = TRUE)
-
-s2.bandx <- lapply(sr_allBands_matrix, "[", ,seasons_ind[["s2"]])
-s2.med <- lapply(s1.bandx, rowMedians, na.rm = TRUE)
-
-s3.bandx <- lapply(sr_allBands_matrix, "[", ,seasons_ind[["s3"]])
-s3.med <- lapply(s1.bandx, rowMedians, na.rm = TRUE)
-
-s4.bandx <- lapply(sr_allBands_matrix, "[", ,seasons_ind[["s4"]])
-s4.med <- lapply(s1.bandx, rowMedians, na.rm = TRUE)
-
-
-
-lapply(sr_allBands_matrix, FUN = matrixStats::rowMedians, na.rm = TRUE)
-s1_b1 <- b1_matrix[, s1_ind]
-s1_b1_med <- matrixStats::rowMedians(s1_b1, na.rm = TRUE, dim. = dim(s1_b1))
-
+lat_import <- ncvar_get(ncf, "lat")
+lon_import <- ncvar_get(ncf, "lon")
+sr_import <- ncvar_get(ncf, "sr")
+time_import <- ncvar_get(ncf, "time")
+ncraster <- brick(file_name, varname = "b1_sr")
+#time <- str_extract(b1, '[0-9][0-9][0-9][0-9][0-9][0-9][0-9]') # get doy of each image
